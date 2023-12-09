ea28c16904a3286302e2df684022cf80b21c89d9
LukasHe0908-test <88268059+LukasHe0908-test@users.noreply.github.com>
2023-12-09 22:25:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ async function startMiniflare() {
 }
 
 startMiniflare();
+
+const https = require('https');
+keepalive();
+function keepalive() {
+  // 保持唤醒
+  let url_host = '';
+  url_host = process.env.RENDER_EXTERNAL_HOSTNAME;
+  if (!url_host) return;
+  https
+    .get(`https://${url_host}/generate_204`, res => {
+      if (res.statusCode == 204) {
+      } else {
+        console.log('请求错误: ' + res.statusCode);
+      }
+    })
+    .on('error', err => {
+      console.log('请求错误: ' + err);
+    });
+  setTimeout(() => {
+    keepalive();
+  }, (Math.ceil(Math.random() * 15) * 1000 * 60) / 2);
+}
